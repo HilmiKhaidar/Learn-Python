7ddcd7bcb7edd85399111c5fc71d3cd9ac27d839
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-06-20 22:20:06 +0700

    Day 2 - Variabel & Tipe Data

--- index2.py
+++ index2.py
@@ -1,9 +1,22 @@
-nama = "Mikhaii"
-umur = 21
-tinggi = 170
-mahasiswa = True
+# nama = "Mikhaii"
+# umur = 21
+# tinggi = 170
+# mahasiswa = True
 
-print("Nama saya adalah : " + nama);
-print("Umur saya adalah : " + str (umur));
-print("Tinggi saya adalah : " + str (tinggi));
-print("Saya adalah mahasiswa : " + str (mahasiswa));
+# print("Nama saya adalah : " + nama);
+# print("Umur saya adalah : " + str (umur));
+# print("Tinggi saya adalah : " + str (tinggi));
+# print("Saya adalah mahasiswa : " + str (mahasiswa));
+
+# Info Laptop
+merk = "ASUS"
+ram = 8  # dalam GB
+ssd = 512  # dalam GB
+laptop_gaming = False
+
+print("=== INFO LAPTOP ===")
+print("Merk        :", merk)
+print("RAM         :", ram, "GB")
+print("SSD         :", ssd, "GB")
+print("Laptop Gaming?:", laptop_gaming)
+
