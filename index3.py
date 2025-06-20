cbc9e87803502adbade429cc8c541efff61037ce
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-06-20 22:21:20 +0700

    Day 3 - Operator

---
+++ index3.py
@@ -0,0 +1,10 @@
+a = 10
+b = 5
+
+print("Penjumlahan:", a + b)
+print("Pengurangan:", a - b)
+print("Perkalian:", a * b)
+print("Pembagian:", a / b)
+print("Pembagian bulat:", a // b)
+print("Sisa bagi:", a % b)
+print("Pangkat:", a ** b)
