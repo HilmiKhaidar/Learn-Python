25fba1a6b0a3124693fcaf73723fc128060d7481
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-06-20 22:23:15 +0700

    Day 3 - Operator

--- index3.py
+++ index3.py
@@ -8,3 +8,10 @@ print("Pembagian:", a / b)
 print("Pembagian bulat:", a // b)
 print("Sisa bagi:", a % b)
 print("Pangkat:", a ** b)
+
+umur = int(input("Masukkan umur kamu: "))
+punya_ktp = input("Apakah kamu punya KTP? (ya/tidak): ")
+
+boleh_buat_sim = umur >= 17 and punya_ktp.lower() == "ya"
+
+print("Boleh buat SIM C:", boleh_buat_sim)
