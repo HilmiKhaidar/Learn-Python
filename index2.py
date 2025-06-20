95fa155f65dd8502c1597458a2c063b7401d6e0b
HilmiKhaidar <Hilmikhaidarnabilla@gmail.com>
2025-06-20 22:18:29 +0700

    Day 2 - Variabel & Tipe Data

---
+++ index2.py
@@ -0,0 +1,9 @@
+nama = "Mikhaii"
+umur = 21
+tinggi = 170
+mahasiswa = True
+
+print("Nama saya adalah : " + nama);
+print("Umur saya adalah : " + str (umur));
+print("Tinggi saya adalah : " + str (tinggi));
+print("Saya adalah mahasiswa : " + str (mahasiswa));
